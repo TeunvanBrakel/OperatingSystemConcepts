bcb23247e15975c8664dd6d1daeb3d79365dc771
TeunvanBrakel <teunvbrakel@gmail.com>
2024-10-03 10:09:21 +0200

    added semaphores

--- project2/main.cpp
+++ project2/main.cpp
@@ -26,6 +26,10 @@ enum BufferState {
   Unbounded = static_cast<std::size_t>(-1)
 };
 
+std::mutex logSemaphore;
+std::mutex bufferSemaphore;
+std::mutex boundSemaphore;
+
 //I want this to be a pair of <LogType, std::string> however I do not know if this allowed so I won't :(
 std::stack<std::string> logs;
 std::vector<int32_t> buffer; 
@@ -61,70 +65,113 @@ const std::string FormatSuccesMessage(const int item, std::string custom_message
 std::map<std::size_t, std::function<const std::string(int)>> SuccesMessages = {
     {ADDED_TO_BUFFER, [] (const int item) { return FormatSuccesMessage(item, "Item added to buffer" ); } },
     {REMOVED_FROM_BUFFER, [] (const int item) { return FormatSuccesMessage(item, "Item removed from bufer"); }},
-    {SET_BOUND, [] (const int item) { return FormatSuccesMessage(item, "set as bound"); }},
+    {SET_BOUND, [] (const int item) { return FormatSuccesMessage(item, " set as bound"); }},
     {UNBOUND, [] (const int _) { return FormatSuccesMessage(_, "Unbounded buffer");}}
 };
 void write_to_log(const std::string& s) {
-  logs.push(s);
+  if(logSemaphore.try_lock()){
+    logs.push(s);
+    logSemaphore.unlock();
+  }
+  
 }
 
-std::string read_from_string(std::size_t element) {
-  if(logs.empty()) {
-    write_to_log(ErrorMessages[NO_LOGS_AVAILABLE]);
-    return "";
+std::string read_from_string() {
+  if(logSemaphore.try_lock()){
+    if(logs.empty()) {
+        write_to_log(ErrorMessages[NO_LOGS_AVAILABLE]);
+        logSemaphore.unlock();
+        return "";
+    }
+    string result = logs.top();
+    logSemaphore.unlock();
+    return result;
   }
-  return logs.top();
 }
 
 void add_to_buffer(int32_t element) {
-  if(BUFFER_SIZE != BufferState::Unbounded && BUFFER_SIZE <= buffer.size()) {
-    write_to_log(ErrorMessages[FULL_BUFFER]);
-    return; 
+  if(boundSemaphore.try_lock()){
+    if(BUFFER_SIZE != BufferState::Unbounded && BUFFER_SIZE <= buffer.size()) {
+      logSemaphore.lock();
+      write_to_log(ErrorMessages[FULL_BUFFER]);
+      logSemaphore.unlock();
+      return; 
+    }
+    logSemaphore.lock();
+    bufferSemaphore.lock();
+    write_to_log(SuccesMessages[ADDED_TO_BUFFER](element));
+    buffer.emplace_back(element);
+    bufferSemaphore.unlock();
+    logSemaphore.unlock();
+    boundSemaphore.unlock();
   }
-  write_to_log(SuccesMessages[ADDED_TO_BUFFER](element));
-  buffer.emplace_back(element);
 }
 
 int32_t remove_from_buffer() {
-  if(buffer.empty()) {
-    write_to_log(ErrorMessages[EMPTY_BUFFER]);
-    return -1;
+  if(bufferSemaphore.try_lock()){
+    if(buffer.empty()) {
+      logSemaphore.lock();
+      write_to_log(ErrorMessages[EMPTY_BUFFER]);
+      logSemaphore.unlock();
+      bufferSemaphore.unlock();
+      return -1;
+    }
+    logSemaphore.lock();
+    int32_t r = buffer.front();
+    buffer.erase(buffer.begin());
+    write_to_log(SuccesMessages[REMOVED_FROM_BUFFER](r));
+    logSemaphore.unlock();
+    bufferSemaphore.unlock();
+    return r; 
   }
-  int32_t r = buffer.front();
-  buffer.erase(buffer.begin());
-  write_to_log(SuccesMessages[REMOVED_FROM_BUFFER](r));
-  return r; 
 }
 
 void set_bound_buffer(size_t bound) {
-  if(bound == BufferState::Unbounded) {
-    write_to_log(ErrorMessages[NEGATIVE_BUFFER_BOUND]);
-    return;
-  }
+  if(boundSemaphore.try_lock()){
+    if(bound == BufferState::Unbounded) {
+      logSemaphore.lock();
+      write_to_log(ErrorMessages[NEGATIVE_BUFFER_BOUND]);
+      logSemaphore.unlock();
+      boundSemaphore.unlock();
+      return;
+    }
 
-  if(bound < buffer.size()) {
-    write_to_log(ErrorMessages[REQUESTED_BOUND_TO_LOW]);
-    return;
+    if(bound < buffer.size()) {
+      logSemaphore.lock();
+      write_to_log(ErrorMessages[REQUESTED_BOUND_TO_LOW]);
+      logSemaphore.unlock();
+      boundSemaphore.unlock();
+      return;
+    }
+    logSemaphore.lock();
+    bufferSemaphore.lock();
+    BUFFER_SIZE = bound;
+    buffer.reserve(bound);
+    write_to_log(SuccesMessages[SET_BOUND](bound));
+    bufferSemaphore.unlock();
+    logSemaphore.unlock();
+    boundSemaphore.unlock();
   }
-
-  BUFFER_SIZE = bound;
-  buffer.reserve(bound);
-  write_to_log(SuccesMessages[SET_BOUND](bound));
 }
 
 void unbound_buffer() {
-  BUFFER_SIZE = -1;
-  write_to_log(SuccesMessages[UNBOUND](-1));
+  if(boundSemaphore.try_lock()){
+    logSemaphore.lock();
+    BUFFER_SIZE = -1;
+    write_to_log(SuccesMessages[UNBOUND](-1));
+    logSemaphore.unlock();
+    boundSemaphore.unlock();
+  }
 }
 
 
 int main(int argc, char* argv[]) {
   remove_from_buffer();
-  std::cout<< read_from_string(0) << endl;
+  std::cout<< read_from_string() << endl;
   string s = "test";
   write_to_log(s);
   set_bound_buffer(9);
-  std::cout << read_from_string(0)<<endl;
+  std::cout << read_from_string()<<endl;
   for(size_t i = 0; i <= 10; ++i) {
     add_to_buffer(i);
   }
@@ -136,6 +183,4 @@ int main(int argc, char* argv[]) {
   set_bound_buffer(-1);
   
 	return 0;
-  
- return 0;
 }
