12e893ae2e66dca3c0945485871befd82f97359f
TeunvanBrakel <teunvbrakel@gmail.com>
2024-10-11 09:41:53 +0200

    Added tests from Justin

--- project2/main.cpp
+++ project2/main.cpp
@@ -147,11 +147,11 @@ Result<std::string, std::string> read_from_log() {
     read_from_log_allowed_mtx.unlock();
     while(n_readers > 0) { //Critical section because of n_readers
       if(logs.empty()) { //Critical section
-          write_to_log(ErrorMessages[NO_LOGS_AVAILABLE]);
           --n_readers; //Critical section
+          write_to_log(ErrorMessages[NO_LOGS_AVAILABLE]);
           return Err<std::string>("Log is empty");
       }
-      string result = logs.top(); //Critical section
+      string result = logs.top(); //No critical section because the writers wait until n_readers is 0.
       --n_readers; //Critical section
       return Ok(result);
     }
@@ -218,39 +218,199 @@ void unbound_buffer() {
 
 
 /* TESTING FUNCTIONS */
-void test_buffer_integrity(std::size_t expected_size) {
-    assert(buffer.size() == expected_size);
-
-    std::unordered_set<int> unique_values(buffer.begin(), buffer.end());
-    assert(unique_values.size() == expected_size);
-
-    for (std::size_t i = 0; i < expected_size; ++i) {
-        assert(unique_values.find(i) != unique_values.end()); 
-    }
-
-    for(std::size_t i = 0; i < expected_size; ++i) {
-      std::cout << buffer[i] << "\n";
-    }
-    std::cout << endl;
-    
-    std::cout << "Buffer integrity test passed!" << std::endl;
+void report_test_result(const std::string& test_name, bool passed) {
+    const char* color_code = passed ? "\033[1;32m" : "\033[1;31m";
+    const char* reset_code = "\033[0m";
+    std::cout << "===== " << test_name << " " << color_code << (passed ? "PASSED" : "FAILED") << reset_code << " =====\n" << std::endl;
 }
 
-std::size_t run_threads_for_testing(std::size_t n_threads) {
+void test_buffer_integrity(std::size_t n_threads) {
+  std::vector<std::thread> threads;
+  for (std::size_t i = 0; i < n_threads; ++i) {
+      threads.push_back(std::move(std::thread(add_to_buffer, i)));
+  }
+
+  for (auto& th : threads) {
+      th.join();
+  }
+
+  const bool res = buffer.size() == n_threads;
+  assert(res);
+
+  report_test_result("Buffer integrity" , res);
+}
+
+void test_log_integrity(std::size_t expected_size) {
+    const bool res = (logs.size() == expected_size);
+    assert(res);
+  
+    report_test_result("Test log integrity", res);
+}
+
+void test_reading_and_writing_to_logs(std::size_t n_threads) {
+  std::vector<std::thread> threads;
+  std::vector<std::size_t> result_logs;
+
+  while(!logs.empty()) 
+    logs.pop();
+
+  for (std::size_t i = 0; i < n_threads; ++i) {
+        threads.emplace_back([i]() {
+            write_to_log(std::to_string(i));
+        });
+
+        if (i % 10 == 0) {
+            threads.emplace_back([&]() {
+                const std::size_t t = static_cast<std::size_t>(std::stoi(read_from_log().unwrap()));
+                result_logs.emplace_back(t);
+                std::cout << "Read from buffer returned: " + std::to_string(t) << std::endl;
+            });
+        }
+    }
+
+
+    for (auto& th : threads) {
+        if (th.joinable()) {
+            th.join();
+        }
+    }
+
+    const bool res = std::all_of(result_logs.begin(), result_logs.end() , [](std::size_t i) {return i % 10 == 0;});
+
+    report_test_result("Read from log ", res);
+}
+
+void test_writing_and_removing_from_buffer(std::size_t n_threads) {
+  std::vector<std::thread> threads;
+
+  buffer.clear();
+
+  for (std::size_t i = 0; i < n_threads; ++i) {
+        threads.emplace_back([i]() {
+            add_to_buffer(i);
+        });
+
+        if (i % 10 == 0) {
+            threads.emplace_back([]() {
+                std::cout << "Removed from buffer returned: " + std::to_string(remove_from_buffer().unwrap()) << std::endl;
+            });
+        }
+    }
+
+    for (auto& th : threads) {
+        if (th.joinable()) {
+            th.join();
+        }
+    }
+
+    const bool res = buffer.size() == (n_threads - 5);
+    assert(res);
+    
+    report_test_result("Buffer Write/Read test ", res); 
+}
+
+void run_write_to_logs_setup(std::size_t n_threads) {
     std::vector<std::thread> threads;
+
+    while(!logs.empty())
+      logs.pop();
+
     for (std::size_t i = 0; i < n_threads; ++i) {
-        threads.push_back(std::thread(add_to_buffer, i));
+        threads.push_back(std::move(std::thread(write_to_log, std::to_string(i))));
     }
 
     for (auto& th : threads) {
         th.join();
     }
-    return n_threads;
 }
 
+void test_empty_read_from_log_returns_err() {
+  while(!logs.empty()) 
+    logs.pop();
+
+  const bool res = read_from_log().is_err();
+  assert(res);
+  report_test_result("Test empty log returns error", res);
+}
+
+void intergration_test_for_only_deadlocks(std::size_t n_threads) {
+  std::vector<std::thread> threads;
+
+  //Setup
+  while(!logs.empty()) 
+    logs.pop();
+  buffer.clear();
+
+  const std::function<void(const std::string message, const std::size_t i)> print = 
+    [](const std::string message, const std::size_t i) {
+      std::cout << message << ", on iteration:" << std::to_string(i) << std::endl;
+    };
+
+  for (std::size_t i = 0; i < n_threads; ++i) {
+       if (i == 1) {
+          threads.emplace_back([&]() {
+              print("Unbound buffer", i);
+              unbound_buffer();
+        });
+      }
+
+      if (i == n_threads - 1) {
+          threads.emplace_back([&]() {
+              print(("Bound buffer to " + std::to_string(i)), i);
+              set_bound_buffer(n_threads);
+          });
+      }
+
+       if (i % 3 == 0) {
+            threads.emplace_back([&]() {
+                add_to_buffer(static_cast<int32_t>(i));
+                print(("Item i added to buffer: " + std::to_string(i)), i);
+            });
+        }
+
+        if (i % 6 == 0) {
+            threads.emplace_back([&]() {
+                const int32_t t =  remove_from_buffer().unwrap();
+                print(("Item i added to buffer: " + std::to_string(t)), i);
+            });
+        }
+
+      if (i % 4 == 0) {
+          threads.emplace_back([&]() {
+              print("Written value: " + std::to_string(i) + " to log", i);
+              write_to_log(std::to_string(i));
+          });
+      }
+
+
+        if (i % 5 == 0) {
+            threads.emplace_back([&]() {
+                const std::string t = read_from_log().unwrap();
+                print(("Read from buffer returned: " + t), i);
+            });
+        }
+    }
+
+    for (auto& th : threads) {
+        if (th.joinable()) {
+            th.join();
+        }
+    }
+
+    //You made it here, so this time no deadlock :)
+    report_test_result("Read from log ", true);
+}
 
 int main(int argc, char* argv[]) {
-  auto n = run_threads_for_testing(100);
-  test_buffer_integrity(n);
+  const std::size_t N_THREADS = 50;
+  test_buffer_integrity(N_THREADS);
+  test_log_integrity(N_THREADS);
+  test_writing_and_removing_from_buffer(N_THREADS);
+
+  test_reading_and_writing_to_logs(N_THREADS);
+  run_write_to_logs_setup(N_THREADS);
+  test_log_integrity(N_THREADS);
+  test_empty_read_from_log_returns_err();
+  intergration_test_for_only_deadlocks(100);
 	return 0;
 }
